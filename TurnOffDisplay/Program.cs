c4cda2148bd97ece8b17256a90b7aa0144aa155a
Diogo Soares <info@diogosoares.ca>
2014-07-31 21:16:19 -0400

    Added GPL v2 header

--- TurnOffDisplay/Program.cs
+++ TurnOffDisplay/Program.cs
@@ -1,4 +1,24 @@
-﻿using System.Runtime.InteropServices;
+﻿/*
+ * TurnOffDisplay
+ * 
+ * Copyright (C) 2014 Diogo Soares
+ * 
+ * This file is part of TurnOffDisplay.
+ * 
+ * TurnOffDisplay is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * TurnOffDisplay is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with PhotoFrame. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Authors: Diogo Soares
+ */
+
+using System.Runtime.InteropServices;
 using System.Threading;
 using System;
 using System.IO;
